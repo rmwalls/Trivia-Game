90baf4e5793d84ed46ae5a8c445905f432e9a24a
ZelleM <groovyrlm@gmail.com>
2019-05-20 17:50:05 -0400

    Just need to make restart work and tweak some CSS!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,17 +6,16 @@ var gameStats = {
 } //end gameStats
 
 var intervalId; //holds the intervalID
-var number = 24; //Set counter countdown 
+var number = 11; //Set counter countdown 
+$("#showQA").hide();
 
-// When the button is clicked, the timer starts. Eventually the 1st quest will also display
+// When button is clicked run function timer
 $("#button-start").click(timer); 
 
-// start timer, fade the button away, display 1st question    
+// start timer, hide button, display 1st question    
 function timer() {
-    $("#button-start").fadeOut(100);
-    clearInterval(intervalId);  
+    $("#button-start").hide();
     intervalId = setInterval(decrement, 1000);
-    decrement();
     displayQandA();
 } //end timer
 
@@ -24,9 +23,12 @@ function timer() {
 function decrement() {
     number--;
     $("#timer").html("<h2>" + number + " seconds left" + "</h2>"); //Display timer on page
-        if (number === 0) { //Once number hits zero, run stop 
+        if (number === 0) { //Once number hits zero, stop the timer 
             stop();
-            console.log("Time's Up!");
+	    $("#resultMessage").show();
+	    $("#resultMessage").html("<h2><p>Oops! Time's Up! <br> The correct answer was: <br>" + QuestionsArr[indexQuestion].answer + "</p></h2>");
+	    gameStats.unanswered++;
+            setTimeout(nextQuestion, 2000);
         } //end if
 } //end decrement
 
@@ -36,33 +38,27 @@ function stop() {
 
 function restartTimer() {
     clearInterval(intervalId);
+    number=11;
     intervalId = setInterval(decrement, 1000);
-}
+} //end restartTimer
 
 // object for 1st question
 let q1 = {
     qNumber: 1, 
     question: "The founder of Netscape was:",
-    answer: "B. Marc Andreesen",
     choices: ["A. Al Gore", "B. Marc Andreesen", "C. Steve Case", "D. Elon Musk"],
+    answer: "B. Marc Andreesen",
     correctAnswer: 1,
     image: "assets/images/andreessen.jpg"
-        
-   // working(){
-     //   $(".qNumb").html("Question # " + Q1.qNumber + " of 10");
-    //$("#questionText").html("<h3>" + Q1.question);
-    //$("#answerChoices").html(this.choices[0] + this.choices[1] + this.choices[2] + this.choices[3]);
-   // } //end working
 } // end Q1
 
-//Q1.working();
 
 // object for question2
 let q2 = {
     qNumber: 2, 
     question: "The M in HTML stands for:",
-    answer: "C. Markup",
     choices: [" A. Marking ", " B. Markle ", " C. Markup ", " D. Marketing "],
+    answer: "C. Markup",
     correctAnswer: 2,
     image: "assets/images/markup.jpg"
 }
@@ -71,8 +67,8 @@ let q2 = {
 let q3 = {
     qNumber: 3, 
     question: "CSS stands for:",
-    answer: "A. Cascading Style Sheets",
     choices: [" A. Cascading Style Sheets ", " B. Clever Style Sheets ", " C. Collapsing Style Sheets ", " D. Cascading Style Syntax "],
+    answer: "A. Cascading Style Sheets",
     correctAnswer: 0,
     image: "assets/images/css.jpg"
 }
@@ -81,8 +77,8 @@ let q3 = {
 let q4 = {
     qNumber: 4, 
     question: "Bootstrap is owned by:",
-    answer: "D. Twitter",
     choices: [" A. Microsoft ", " B. Facebook ", " C. Sun Microsystems ", " D. Twitter "],
+    answer: "D. Twitter",
     correctAnswer: 3,
     image: "assets/images/bootstrap.jpg"
 }
@@ -91,18 +87,18 @@ let q4 = {
 let q5 = {
     qNumber: 5, 
     question: "Which of the following is NOT a web browser?",
-    answer: "C. SpeedWeb",
     choices: [" A. SeaMonkey ", " B. Lynx ", " C. SpeedWeb ", " D. Voyager "],
+    answer: "C. SpeedWeb",
     correctAnswer: 2,
-    image: ""
+    image: "assets/images/browser.jpg"
 }
 
 // object for question6
 let q6 = {
     qNumber: 6, 
     question: "JAVA stands for:",
-    answer: "A. Nothing, it's a reference to coffee",
-    choices: [" A. Nothing, it's a reference to coffee ", " B. Just-in-time All-Code Verification Application ", " C. James’s Application Virtual Applets ", " D. Justified Automatic Version Anywhere "],
+    choices: [" A. Nothing, it's a reference to coffee ", " B. Just-in-time All-Code Verification Application ", " C. James's Application Virtual Applets ", " D. Justified Automatic Version Anywhere "],
+    answer: "A. Nothing, it's a reference to coffee",    
     correctAnswer: 0,
     image: "assets/images/java.jpg"
 }
@@ -111,8 +107,8 @@ let q6 = {
 let q7 = {
     qNumber: 7, 
     question: "Which is NOT a shade of white recognized in HTML?",
-    answer: "D.	LilyWhite",
     choices: [" A. AntiqueWhite ", " B. GhostWhite ", " C. FloralWhite ", " D. LilyWhite "],
+    answer: "D.	LilyWhite",
     correctAnswer: 3,
     image: "assets/images/white.jpg."
 }
@@ -121,8 +117,8 @@ let q7 = {
 let q8 = {
     qNumber: 8, 
     question: "Which was NOT the name of an early internet search engine?",
-    answer: "A. Betty",
     choices: [" A. Betty ", " B. Veronica ", " C. Archie ", " D. Jughead "],
+    answer: "A. Betty",
     correctAnswer: 0,
     image: "assets/images/search.jpg"
 }
@@ -131,18 +127,20 @@ let q8 = {
 let q9 = {
     qNumber: 9, 
     question: "This is/was the name of an HTML editor:",
-    answer: "B. Hotdog",
     choices: [" A. HTML-it! ", " B. Hotdog ", " C. Web-A-Roo ", " D. FrameItUp "],
+    answer: "B. Hotdog",
     correctAnswer: 1,
     image: "assets/images/hotdog.jpg"
 }
 
 
-//Array of questions
+//Array of questions to cycle through
 let QuestionsArr = [q1, q2, q3, q4, q5, q6, q7, q8, q9];
 var indexQuestion = 0;
 
 function displayQandA() {
+	console.log("I got to function displayQanA");
+        $("#showQA").show();
 	$("#questionText").html("<h3>" + QuestionsArr[indexQuestion].question + "</h3>");
 	$("#button0").text(QuestionsArr[indexQuestion].choices[0]);
 	$("#button1").text(QuestionsArr[indexQuestion].choices[1]);
@@ -159,35 +157,30 @@ function displayQandA() {
             
             //Check for right/wrong
             if (userButtonValue == QuestionsArr[indexQuestion].correctAnswer) {
-                $("#resultMessage").html("<h2><p>Correct!</p></h2><img src='" + QuestionsArr[indexQuestion].image + "' height = 200 width = 350 alt='correct'>");
-                gameStats.right ++;//increment score
-                console.log("correct answer " + gameStats.right);
+		$("#questionText").hide();
+		$("#resultMessage").show();
+        $("#resultMessage").html("<h2><p>Correct! <br> the answer is "  + QuestionsArr[indexQuestion].answer + "<img src='" + QuestionsArr[indexQuestion].image + "' height = 200 width = 350 alt='Correct'>");
+                gameStats.right ++; //increment score
+                console.log("correct answers now = " + gameStats.right);
                 //audio = new Audio("assets/sounds/ding.mp3");
                 //audio.play();
-                
-                //reset timer
-                stop();
-                restartTimer();						
+                stop(); //stop timer              					
 
             } else {
-            
-                $("#resultMessage").html("<h2><p>Oops! Wrong! <br> The correct answer was: <br>" + QuestionsArr[indexQuestion].answer + "</p></h2>");
+            	$("#resultMessage").show();
+                $("#resultMessage").html("<h2><p>Oops! Wrong! <br> The correct answer was: <br>" + QuestionsArr[indexQuestion].answer);
                 gameStats.wrong ++;
-                console.log("wrong answer " + gameStats.wrong);
+                console.log("wrong answers now = " + gameStats.wrong);
                 //audio = new Audio("assets/sounds/Buzzer.mp3");
                 //audio.play();
-
-                //reset timer
-                stop();
-                restartTimer();	
+                stop();	//stop timer
             } //end second if
 
-            $("#yourResult").show(); //show the correct image div
             $(".timer").hide();
-            $("#gameOver").hide();
+            $(".gameOver").hide();
             $(".btn").hide();
 
-            setTimeout(nextQuestion, 3000);
+            setTimeout(nextQuestion, 2000);
             
         } //end first if
     }); 
@@ -197,22 +190,31 @@ function displayQandA() {
     
         if (indexQuestion < QuestionsArr.length) {
             displayQandA();
-            $(".yourResult").hide();
+	$("#questionText").show();
+            $("#resultMessage").hide();
             $("#timerDisplay").show();
             $(".btn").show();
             stop();
-            //timer.reset();
             restartTimer();
         } else	{ //Display scores when game ends
-            $(".yourResult").hide();
-            $("#question").hide();
-            $("#score").html("<div>"+ "Game Over! <br> Your Score" +"</div>"+
+	console.log("Got to game ends");
+            $("#showQA").hide();
+	    $(".gameOver").show();
+            $(".gameOver").html("<div>"+ "Game Over! <br> Your Score" +"</div>"+
             "<div>"+ "Correct Answers: " + gameStats.right +"</div>" + 
             "<div>"+ "Wrong Answers: " + gameStats.wrong +"</div>" +
             "<div>"+ "Unanswered Questions: " + gameStats.unanswered +"</div>");
-            $(".gameOver").html('<button type="reset" class="restartMe" id="reset" value=reset>Restart Game</button>');
+           $(".gameOver").append('<button type="reset" class="restartMe" id="reset" value=reset>Restart Game</button>');
+	$("#restartMe").click(restartGame); 
         } //end if
     } //end nextQuestion
     
+function restartGame() {
+	gameStats.right=0;
+	gameStats.wrong=0;
+	gameStats.unanswered=0;
+	indexQuestion=0;
+	timer();
+} // end restartGame
 
 }); //end document.ready
---
+++ assets/javascript/pseudo.js
@@ -0,0 +1,94 @@
+// 5/19/19 TODO still
+//make restart button work
+//question number on page
+//if time runs out on the last question, it bombs
+// work on css of image size
+// make timer restart correctly DONE
+//clear out previous response and answer info DONE
+// work on css of text DONE
+//get image for question 5 DONE
+// deal with no answer/expired time situation DONE
+
+
+//BONUS
+// get sounds working
+//fix width of container DONE
+
+
+
+// Pick a theme  DONE
+// Initially, page has a Start button DONE
+// upon clicking start, DONE
+// Start button goes away DONE
+//count down timer shown DONE
+// Show question number (1 of 10, etc,)
+//(restarts with each question)
+    // need questions each with 1 correct answer & 3 wrong answers - object DONE
+    // need image for each correct answer DONE
+// show first question, with 4 answers to choose from DONE
+    // only allow one selection DONE (via click event
+    // as soon as answer is clicked, 
+        // stop timer  DONE
+        // determine if userAnswer is correct DONE
+        // show right or wrong DONE
+        // show correct answer with image DONE
+        // store count of right, wrong answers, get # of unanswered questions DONE
+        // after a few seconds DONE
+            //reset timer and redisplay DONE
+            //display new question with answers DONE
+    // if time expires before question is answered
+        // stop timer DONE
+        // display "Time's up" message DONE
+        // display correct answer DONE
+        // increase unanswered count DONE
+        // after a few seconds DONE
+            //reset timer and redisplay DONE
+            // display new questions with answers DONE
+    //After final question
+        //stop timer DONE
+        //display a message DONE
+        //display # of right, wrong and unanswered questions DONE
+        //provide Play Again button
+            //reset parameters
+            //restart game
+
+
+// Thoughts:
+// use classes
+    // questions
+    // answers - assign numbers to answers to ease comparisons (right/wrong)
+    // images
+// Potential functions
+    // startGame
+    // resetGame
+    // displayCorrectAnwer
+    // showStats
+    // displayQuestions
+    // determineResult
+// Timers needed
+    //per question DONE
+    //while displaying result DONE
+// When Start is pressed, the page content is just replaced, doesn't go to a new page DONE
+
+// Each question needs to have:
+    // 3 wrong answers
+    // 1 right answer
+    // image 
+    // sounds
+
+//Image list
+//andreeseen.jpg
+//markup.jpg
+//css.jpg
+//bootstrap.jpg
+//java.jpg
+//white.jpg
+//search.jpg
+//hotdog
+//
+//
+
+//what's what
+//yourResult is 
+//gameOver needs to have Replay button and Game Over msg
+//resultMessage=You're Right or Oops Wrong
